16c6b2ba0ec4dc4e731028f92fd3287776bf355d
mutantmonkey <mutantmonkey@mutantmonkey.in>
2016-03-14 20:43:55 -0700

    clean up client
    * Add some more useful comments
    * Remove some unnecessary comments
    * Remove an unnecessary goroutine

--- client/client.go
+++ client/client.go
@@ -14,6 +14,7 @@ import (
 	"golang.org/x/crypto/ssh"
 )
 
+// compare received server key with configured server key
 func checkServerKey(conn ssh.ConnMetadata, key ssh.PublicKey) (*ssh.Permissions, error) {
 	receivedKey := strings.TrimSpace(string(ssh.MarshalAuthorizedKey(key)))
 
@@ -48,12 +49,7 @@ func main() {
 
 	go ssh.DiscardRequests(reqs)
 
-	// Service the incoming Channel channel.
 	for newChannel := range chans {
-		// Channels have a type, depending on the application level
-		// protocol intended. In the case of a shell, the type is
-		// "session" and ServerShell may be used to present a simple
-		// terminal interface.
 		if newChannel.ChannelType() != "session" {
 			newChannel.Reject(ssh.UnknownChannelType, "unknown channel type")
 			continue
@@ -63,9 +59,34 @@ func main() {
 			log.Fatal("Could not accept channel: %v", err)
 		}
 
-		// Sessions have out-of-band requests such as "shell",
-		// "pty-req" and "env".  Here we handle only the
-		// "shell" request.
+		shell := exec.Command(defaultShell)
+
+		teardown := func() {
+			channel.Close()
+			_, err := shell.Process.Wait()
+			if err != nil {
+				log.Printf("Failed to exit shell: %v", err)
+			}
+			log.Print("Session closed")
+			os.Exit(0)
+		}
+
+		shellf, err := pty.Start(shell)
+		if err != nil {
+			log.Fatalf("Failed to spawn shell: %v", err)
+		}
+
+		// connect the pipes
+		var once sync.Once
+		go func() {
+			io.Copy(channel, shellf)
+			once.Do(teardown)
+		}()
+		go func() {
+			io.Copy(shellf, channel)
+			once.Do(teardown)
+		}()
+
 		go func(in <-chan *ssh.Request) {
 			for req := range in {
 				ok := false
@@ -73,44 +94,13 @@ func main() {
 				case "shell":
 					ok = true
 					if len(req.Payload) > 0 {
-						// We don't accept any
-						// commands, only the
-						// default shell.
+						// only the default shell
+						// is allowed
 						ok = false
 					}
 				}
 				req.Reply(ok, nil)
 			}
 		}(requests)
-
-		go func() {
-			shell := exec.Command(defaultShell)
-
-			teardown := func() {
-				channel.Close()
-				_, err := shell.Process.Wait()
-				if err != nil {
-					log.Printf("Failed to exit shell: %v", err)
-				}
-				log.Print("Session closed")
-				os.Exit(0)
-			}
-
-			shellf, err := pty.Start(shell)
-			if err != nil {
-				log.Fatalf("Failed to spawn shell: %v", err)
-			}
-
-			// connect the pipes
-			var once sync.Once
-			go func() {
-				io.Copy(channel, shellf)
-				once.Do(teardown)
-			}()
-			go func() {
-				io.Copy(shellf, channel)
-				once.Do(teardown)
-			}()
-		}()
 	}
 }
