534ddec71aa4e250293b551b10fd5d2affc7cd09
mutantmonkey <mutantmonkey@mutantmonkey.in>
2018-11-26 13:56:14 -0800

    Update ldflags in build script

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ m4 config.m4 client/config.go.in > client/config.go
 m4 config.m4 server/config.go.in > server/config.go
 
 mkdir -p out
-go build -o out/client mutantmonkey.in/code/grrsh/client
-go build -o out/server mutantmonkey.in/code/grrsh/server
+go build -ldflags="-s -w" -o out/client mutantmonkey.in/code/grrsh/client
+go build -ldflags="-s -w" -o out/server mutantmonkey.in/code/grrsh/server
